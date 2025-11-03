8066af86b7cb97c907a89f04cbbaa481b109ed20
Hugo Perdomo <hugo@perdomo.dev>
2025-11-03 10:40:18 -0500

    feat(zettelkasten): add note creation keymap and module
    * Introduced <leader>fz keymap to create new Zettelkasten notes
    * Added detection for "00 - Zettelkasten" directory

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local zettel_dir = vim.fn.finddir("00 - Zettelkasten", vim.fn.getcwd() .. ";")
+
+if zettel_dir ~= "" then
+  local zettel = require("custom.zettelkasten")
+  vim.keymap.set("n", "<leader>fz", zettel.create_note, { desc = "Create Zettelkasten note" })
+end
---
+++ lua/custom/zettelkasten.lua
@@ -0,0 +1,21 @@
+local M = {}
+
+function M.create_note()
+  vim.ui.input({ prompt = "Note name: " }, function(input)
+    if not input or input == "" then
+      return
+    end
+
+    local filename = input:match("%.md$") and input or input .. ".md"
+    local zettel_dir = vim.fn.finddir("00 - Zettelkasten", vim.fn.getcwd() .. ";")
+
+    if zettel_dir == "" then
+      vim.notify("0 Zettelkasten folder not found", vim.log.levels.ERROR)
+      return
+    end
+
+    vim.cmd("edit " .. zettel_dir .. "/" .. filename)
+  end)
+end
+
+return M
