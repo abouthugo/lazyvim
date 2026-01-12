b949d7f7d821526f315972a4dccbfe0f59b8a53f
Hugo Perdomo <hugo@perdomo.dev>
2026-01-12 11:14:51 -0500

    update treesitter config with custom parser

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,18 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
-    vim.list_extend(opts.ensure_installed, { "brightscript", "xml" })
+    vim.list_extend(opts.ensure_installed, { "xml" })
+    -- Register custom parser
+    local parser_config = require("nvim-treesitter.parsers")
+    parser_config.brightscript = {
+      install_info = {
+        url = "https://github.com/abouthugo/tree-sitter-brightscript",
+        branch = "feat/brighterscript-support",
+        files = { "src/parser.c" },
+        revision = "4c01e8bb93acefc55311a07a0a849a295c6b72cd",
+      },
+      filetype = "brightscript",
+      tier = 3,
+    }
   end,
 }
