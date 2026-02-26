1d4d54772cb947fd9f19a01190576e28b3d45df6
Hugo Perdomo <hugo@perdomo.dev>
2026-02-26 09:50:59 -0500

    add custom styles for MarkdownPreview

--- init.lua
+++ init.lua
@@ -1,3 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("custom.search")
+
+-- MarkDown Preview custom css
+-- vim.g.mkdp_theme = "light"
+vim.g.mkdp_markdown_css = vim.fn.expand("~/.config/nvim/css/github-markdown.css")
+vim.g.mkdp_highlight_css = vim.fn.expand("~/.config/nvim/css/highlight.css")
